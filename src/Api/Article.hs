37d3510ff94eecec47011e23126523cc18e6f116
shokohara <shokohara@users.noreply.github.com>
2016-10-11 16:58:16 +0900

    Add POST /articles for article list

--- src/Api/Article.hs
+++ src/Api/Article.hs
@@ -7,23 +7,25 @@
 {-# LANGUAGE TypeFamilies               #-}
 
 module Api.Article where
-import           Control.Monad.Except
-import           Control.Monad.Reader        (ReaderT, runReaderT)
-import           Control.Monad.Reader.Class
-import           Data.Int                    (Int64)
-import           Database.Persist.Postgresql
-import           Network.Wai                 (Application)
-import           Servant
-import           Config                      (App (..), Config (..))
-import           Models
+import Config (App (..), Config (..))
+import Control.Monad.Except
+import Control.Monad.Reader (ReaderT, runReaderT)
+import Control.Monad.Reader.Class
+import Data.Int (Int64)
+import Data.Maybe (catMaybes)
+import Database.Persist.Postgresql
+import Models
+import Network.Wai (Application)
+import Servant
 
 type ArticleAPI  = "articles" :> Get '[JSON] [Entity Article]
   :<|> "articles" :> Capture "id" Int64 :> Get '[JSON] (Entity Article)
+  :<|> "articles" :> ReqBody '[JSON] [Article] :> Post '[JSON] [Entity Article]
   :<|> "articles" :> ReqBody '[JSON] Article :> Post '[JSON] (Entity Article)
   :<|> "articles" :> Capture "id" Int64 :> Delete '[JSON] NoContent
 
 articleServer :: ServerT ArticleAPI App
-articleServer = listArticle :<|> findArticle :<|> createArticle :<|> deleteArticle
+articleServer = listArticle :<|> findArticle :<|> createArticles :<|> createArticle :<|> deleteArticle
 
 listArticle :: App [Entity Article]
 listArticle = runDb $ selectList [] []
@@ -37,11 +39,26 @@ findArticle v = do
 
 createArticle :: Article -> App (Entity Article)
 createArticle v = do
-  articleId <- runDb . insert $ Article (articleSkypeId v) (articleName v) (articleTitle v) (articleTags v) (articleBody v) (articleSex v) (articleCreatedAt v)
-  maybeArticle <- runDb $ selectFirst [ArticleId ==. articleId] []
-  case maybeArticle of
-    Nothing -> throwError err404
-    Just x -> return x
+  article <- runDb $ selectFirst [ArticleSkypeId ==. articleSkypeId v, ArticleName ==. articleName v, ArticleTags ==. articleTags v, ArticleBody ==. articleBody v, ArticleSex ==. articleSex v, ArticleCreatedAt ==. articleCreatedAt v] []
+  case article of
+    Nothing -> do
+      articleId <- runDb . insert $ v
+      maybeArticle <- runDb $ selectFirst [ArticleId ==. articleId] []
+      case maybeArticle of
+        Nothing -> throwError err400
+        Just x -> return x
+    Just x -> throwError err400
+
+createArticles :: [Article] -> App [Entity Article]
+createArticles as = do
+  x <- forM as $ \v -> do
+    article <- runDb $ selectFirst [ArticleSkypeId ==. articleSkypeId v, ArticleName ==. articleName v, ArticleTags ==. articleTags v, ArticleBody ==. articleBody v, ArticleSex ==. articleSex v, ArticleCreatedAt ==. articleCreatedAt v] []
+    case article of
+      Nothing -> do
+        articleId <- runDb . insert $ v
+        runDb $ selectFirst [ArticleId ==. articleId] []
+      Just x -> return Nothing
+  return $ catMaybes x
 
 deleteArticle :: Int64 -> App NoContent
 deleteArticle v = do
